4df2799649febb3afb7d2fb2f3c039bfbb532301
i5-650 <77734008+i5-650@users.noreply.github.com>
2024-08-29 10:56:02 +0200

    feat: shared variables

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gopen-port
+
+Simple port scanner in Go.
+
+
+# Usage
+```
+Usage of ./gopen-port:
+  -host string
+    	Host to scan
+  -max int
+    	The maximum port you'd like to scan (default 1024)
+  -min int
+    	The minimum port you'd like to scan (can't be bellow 0)
+  -timeout int
+    	Timeout in millisseconds (default 1000)
+```
+
+
+# Disclaimer
+I'm not responsible for what you will do with such a tool. I made it in order to understand some go concepts.
--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 func main() {
     host := flag.String("host", "", "Host to scan")
     timeout := flag.Int("timeout", 1000, "Timeout in millisseconds")
-    maxPort := flag.Int("max", 1024, "The maximum port you'd like to scan")
+    maxPort := flag.Int("max", 1024, "The maximum port you'd like to scan (can't be higher than 65535)")
     minPort := flag.Int("min", 0, "The minimum port you'd like to scan (can't be bellow 0)")
 
     flag.Parse()
 
-    if len(*host) == 0 || *minPort < 0 {
+    if len(*host) == 0 || *minPort < 0 || *maxPort > 65535 {
         flag.Usage()
         return
     }
@@ -27,20 +27,27 @@ func main() {
     start := time.Now()
 
     var wg sync.WaitGroup
+    var mutex sync.Mutex
+    var openPorts []int
 
     for i := *minPort ;i <= *maxPort; i++ {
         wg.Add(1)
-        go ScanPort(i, *host, *timeout, &wg)
+        go ScanPort(i, *host, *timeout, &wg, &mutex, &openPorts)
     }
     wg.Wait()
 
     elapsed := time.Since(start)
 
+    fmt.Println("[+] Open ports:")
+    for _, p := range openPorts {
+        fmt.Printf("\t- %d\n", p)
+    }
+
     fmt.Printf("[+] Scan done in %s sec", elapsed)
 
 }
 
-func ScanPort(port int, addr string, timeout int, wg *sync.WaitGroup) {
+func ScanPort(port int, addr string, timeout int, wg *sync.WaitGroup, mutex *sync.Mutex, openPorts* []int) {
     defer wg.Done()
     conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", addr, port), time.Duration(timeout) * time.Millisecond)
     if err != nil {
@@ -48,5 +55,7 @@ func ScanPort(port int, addr string, timeout int, wg *sync.WaitGroup) {
     }
     defer conn.Close()
 
-    fmt.Printf("[+] %d Port is open\n", port)
+    *openPorts = append(*openPorts, port)
+
+    // fmt.Printf("[+] %d Port is open\n", port)
 }
